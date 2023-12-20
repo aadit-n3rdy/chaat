8cc4898e7ecafdf3ae6d89b4bd6b27120ed17762
Aadithyaa Eeswaran <eaadithyaa@gmail.com>
2023-12-20 17:12:01 +0530

    Switched to TCP instead of UDP

--- src/main.c
+++ src/main.c
@@ -5,13 +5,28 @@
 #include <netinet/in.h> // sockaddr_in
 #include <arpa/inet.h>  // conversions
 #include <errno.h>
+#include <unistd.h>
 
 #define BUFSIZE 256
 
 int chaat_reciever(int sock) {
 	char buf[BUFSIZE];
+	if (listen(sock, 0)) {
+		printf("Error listening: %s\n", strerror(errno));
+		return -1;
+	}
+	printf("Accepting connection...\n");
+	struct sockaddr_in dest_addr;
+	socklen_t da_len = sizeof(dest_addr);
+	int recv_fd = accept(sock, (struct sockaddr*)&dest_addr, &da_len);
+	if (recv_fd < 0) {
+		printf("Error accepting: %s\n", strerror(errno));
+		return -1;
+	}
+	printf("Accepted connection from %s:%hu\n", inet_ntoa(dest_addr.sin_addr),
+			ntohs(dest_addr.sin_port));
 	while (1) {
-		int len = recv(sock, buf, BUFSIZE, 0);
+		int len = recv(recv_fd, buf, BUFSIZE, 0);
 		buf[len] = '\0';
 		if (len > 0) {
 			printf("%s", buf);
@@ -25,7 +40,11 @@ int chaat_reciever(int sock) {
 	return 0;
 }
 
-int chaat_sender(int sock, const struct sockaddr *dest_addr, socklen_t dest_len) {
+int chaat_sender(int sock, const struct sockaddr_in *dest_addr) {
+	if (connect(sock, (const struct sockaddr*)dest_addr, sizeof(*dest_addr)) == -1) {
+		printf("Error connecting: %s\n", strerror(errno));
+		return -1;
+	}
 	char buf[BUFSIZE];
 	while (1) {
 		fgets(buf, BUFSIZE-1, stdin);
@@ -34,7 +53,7 @@ int chaat_sender(int sock, const struct sockaddr *dest_addr, socklen_t dest_len)
 			buf[slen] = '\n';
 			slen++;
 			buf[slen] = '\0';
-			int len = sendto(sock, buf, strlen(buf)-1, 0, dest_addr, dest_len);
+			int len = send(sock, buf, strlen(buf)-1, 0);
 			if (len < 0) {
 				printf("Sending \"%s\" failed due to error: %s\n", buf, strerror(errno));
 				return -1;
@@ -56,16 +75,13 @@ int main(int argc, char **argv) {
 		return -1;
 	}
 	int self_port = atoi(argv[1]);
-	int sock = socket(AF_INET, SOCK_DGRAM, 0);
+	int sock = socket(AF_INET, SOCK_STREAM, 0);
 	struct sockaddr_in sa;
 
 	sa.sin_family = AF_INET;
-	if (!inet_aton("127.0.0.1", &sa.sin_addr.s_addr)) {
-		printf("Invalid IP address\n");
-		return -1;
-	}
 	sa.sin_port = htons(self_port);
-	if (bind(sock, &sa, sizeof(sa))) {
+	sa.sin_addr.s_addr = INADDR_ANY;
+	if (bind(sock, (struct sockaddr*)&sa, sizeof(sa))) {
 		printf("ERROR: Could not bind, %s\n", strerror(errno));
 		return -1;
 	}
@@ -78,16 +94,19 @@ int main(int argc, char **argv) {
 			printf("Requires destination port number, exiting...\n");
 			// TODO: add close calls where necessary
 		}
-		struct sockaddr_in dest_addr;
-		int other_port = atoi(argv[3]);
 
+		int other_port = atoi(argv[3]);
+		struct sockaddr_in dest_addr;
+
+		dest_addr.sin_family = AF_INET;
 		dest_addr.sin_port = htons(other_port);
-		if (!inet_aton("127.0.0.1", &sa.sin_addr.s_addr)) {
+		if (!inet_aton("127.0.0.1", &dest_addr.sin_addr)) {
 			printf("Invalid dest. IP address\n");
 			return -1;
 		}
+
 		printf("Sending on port %hu:\n", ntohs(sa.sin_port));
-		return chaat_sender(sock, &dest_addr, sizeof(dest_addr));
+		return chaat_sender(sock, &dest_addr);
 	}
 	return 0;
 }
